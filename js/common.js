e68240b4af5ed8c687ec28e09a4782eef0b5b442
ITkah <42772919+ITkah@users.noreply.github.com>
2020-02-01 15:10:24 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
     $(".click-top").on("click", function() {
         $('html, body').animate({ scrollTop: 0 }, '400');
     });
+    $(function() {
+ 
+        $('div.tabs__caption').on('click', 'div:not(.active)', function() {
+          $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+       
+    });
+
+    $(".vido-box").on("click", function(){
+       $(this).removeClass("play-icon");
+    });
+
+    $(".video-play").on("click", function(){
+        $(this)[0].play();
+        $(this).attr('controls',true);
+    });
+
 });
